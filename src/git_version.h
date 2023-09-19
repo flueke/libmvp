bf07f6ca0b59ec53102af6c8b6afd76b9a80970c
Florian Lüke <f.lueke@mesytec.com>
2023-09-19 15:51:26 +0200

    add git version info stuff to libmvp
    Note: I'm not actually sure how this interacts with the parent mvp/mvplab
    git version logic or the child mesytec-mvlc code. They all use the same
    CMake variables but of course are in different scopes.

---
+++ src/git_version.h
@@ -0,0 +1,11 @@
+#ifndef EXTERNAL_LIBMVP_SRC_GIT_VERSION_H
+#define EXTERNAL_LIBMVP_SRC_GIT_VERSION_H
+
+namespace mesytec::mvp
+{
+
+const char *library_version();
+
+}
+
+#endif // EXTERNAL_LIBMVP_SRC_GIT_VERSION_H
